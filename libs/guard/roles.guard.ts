c6baf00f6cb1a57ebc954977a8b163a023241bbd
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-05-31 11:47:17 -0300

    fix(roles.guard): update type annotation for requiredRoles to use Prisma Roles enum to ensure type safety and consistency

--- libs/guard/roles.guard.ts
+++ libs/guard/roles.guard.ts
@@ -1,14 +1,14 @@
 import { ROLES_KEY } from '@core/decorators/roles.decorator';
-import { Role } from '@core/enums/role.enum';
 import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Roles } from '@prisma/client';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [context.getHandler(), context.getClass()]);
+    const requiredRoles = this.reflector.getAllAndOverride<Roles[]>(ROLES_KEY, [context.getHandler(), context.getClass()]);
     if (!requiredRoles) {
       return true;
     }
