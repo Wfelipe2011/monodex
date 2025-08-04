4f2d75dc04796c20c239ce83a9a99c48a5ee5c0b
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-08-04 20:07:43 -0300

    feat(controller): add database connection info to health check endpointIncludes current max connections, active, and idle connections by querying PostgreSQL, providing better insight into database status

--- apps/notifly/src/notifly.controller.ts
+++ apps/notifly/src/notifly.controller.ts
@@ -1,17 +1,29 @@
 import { All, Body, Controller, Get, Post, Query, Req, Res } from '@nestjs/common';
 import { LeadsService } from './leads.service';
 import { WhatsAppWebhook } from './interfaces';
+import { PrismaService } from '@core/infra/prisma/prisma.service';
 
 @Controller()
 export class NotiflyController {
-  constructor(private readonly leadsService: LeadsService) { }
+  constructor(private readonly leadsService: LeadsService, private prisma: PrismaService,) { }
 
   @Get()
   async healthCheck() {
+    const databaseInfo = await this.getDatabaseInfo();
+    const {
+      max_connections,
+      countActive,
+      countIdle,
+    } = databaseInfo
     return {
       message: 'Notifly API is running',
       version: 'v1.0.0',
-      timestamp: new Date().toISOString()
+      timestamp: new Date().toISOString(),
+      database_info: {
+        active: +countActive.toString(),
+        idle: +countIdle.toString(),
+        max_connections: +max_connections,
+      },
     };
   }
 
@@ -32,5 +44,18 @@ export class NotiflyController {
       }
     }
     return res.status(200).json({ status: 'ok' });
+  }
+
+  private async getDatabaseInfo() {
+    const [[{ max_connections }], [{ count: countActive }], [{ count: countIdle }]] = await Promise.all([
+      this.prisma.$queryRaw`show max_connections` as Promise<{ max_connections: string }[]>,
+      this.prisma.$queryRaw`select count(1) from pg_stat_activity where state = 'active' and datname = ${process.env.POSTGRES_DB}` as Promise<{ count: BigInt }[]>,
+      this.prisma.$queryRaw`select count(1) from pg_stat_activity where state = 'idle' and datname = ${process.env.POSTGRES_DB}` as Promise<{ count: BigInt }[]>,
+    ]);
+    return {
+      max_connections,
+      countActive,
+      countIdle
+    };
   }
 }
